b5e193e20141d44c856df5f83dbd4c1c0aed6e1a
Prabu Gugagantha <44075174+blackf8@users.noreply.github.com>
2019-12-27 14:12:33 -0500

    Update newAutomation.java

--- newAutomation.java
+++ newAutomation.java
@@ -1,2 +1,11 @@
 import java.io.*;
 import java.util.*;
+public class newAutomation{
+  public static void main(String[] args){
+    Runtime rt = Runtime.getRuntime();
+    Process pr = rt.exec("netsh wlan show profile");
+    InputStream input = pr.getIntputStream();
+    InputStreamReader inputReader = new InputStreamReader(input);
+    BufferedReader input = new BufferedReader(new InputStreamReader((pr.getInputStream())));
+  }
+}
