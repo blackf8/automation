b27b78fa8e75e98fd8756d5ac4b58b654077dea7
Prabu Gugagantha <44075174+blackf8@users.noreply.github.com>
2019-12-28 13:50:10 -0500

    Currently working on reading the input from the command prompt execution of "netsh wlan show profile"

--- newAutomation.java
+++ newAutomation.java
@@ -7,5 +7,14 @@ public class newAutomation{
     InputStream input = pr.getIntputStream();
     InputStreamReader inputReader = new InputStreamReader(input);
     BufferedReader input = new BufferedReader(new InputStreamReader((pr.getInputStream())));
+    String finalReturn = null;
+    finalReturn = FileToString(input);
+  }
+  public static String FileToString(BufferedReader input){
+    String current = null;
+    while((current = input.readLine()) != null){
+
+      
+    }
   }
 }
